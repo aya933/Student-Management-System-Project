ccef1ddfb6de48d0ecec0f806cfef25e6d2ea422
aya933 <82770278+aya933@users.noreply.github.com>
2022-05-21 21:44:42 +0200

    commit
    push baby push

--- projectCollege-4/src/main/java/com/management/college/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.management.college.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.management.college.entity.Person;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-
-}
